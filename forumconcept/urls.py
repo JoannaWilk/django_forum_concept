6c3e34195e1a8ba37dec59d35de16eeaccb24d47
JoannaWilk <wielki.wilq@gmail.com>
2018-06-20 02:23:54 +0200

    add logic for creating posts
    * add Post model
    * add create.html page
    * add home.html page
    * add initial migration file

--- forumconcept/urls.py
+++ forumconcept/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from posts import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^useraccounts/', include('useraccounts.urls')),
     url(r'^posts/', include('posts.urls')),
+    url(r'^$', views.home, name="home"),
 ]
